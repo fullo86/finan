81ef0bdabfa570ab7c455ab86448e3c4386b8a7a
fullo <116483866+fullo86@users.noreply.github.com>
2023-12-06 02:58:27 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "./src/**/*.html", "./src/**/*.js"],
+  content: ["index.html", "./node_modules/flowbite/**/*.js"],
   darkMode: "class",
   theme: {
+    // container: {
+    //   center: true,
+    //   padding: "16px",
+    // },
     extend: {
       screens: {
         "2xl": "1320px",
